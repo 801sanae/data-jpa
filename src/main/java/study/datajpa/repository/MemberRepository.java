56d4847aa10db7c4c6f2e4aaed499ebedf357542
KMY <kyobolife.ymk@gmail.com>
2023-10-31 10:35:08 +0900

    Spring Data JPA NamedQuery 사용 방법 1. @Query Annotation 사용 2. metheod명으로 호출

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -9,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //JpaRepository<Entity, PK의 type>
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //메소드 이름을 분석해서 JPQL을 생성하고 실행
+
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String name);
 }
