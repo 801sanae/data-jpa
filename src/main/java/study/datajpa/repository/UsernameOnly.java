01298c91f514e289b50622cb1b119a7013c759d6
KMY <kyobolife.ymk@gmail.com>
2023-12-15 17:13:09 +0900

    Open Projection , Close Projection (interface 기반)

--- src/main/java/study/datajpa/repository/UsernameOnly.java
+++ src/main/java/study/datajpa/repository/UsernameOnly.java
@@ -1,5 +1,14 @@
 package study.datajpa.repository;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface UsernameOnly {
+
+    // Open projection,,
+    // Entity에서 모두 가져온후 처리
+//    @Value("#{target.username + ' ' + target.age}")
+
+    // close projection,,
+    // 명확하게 필요한 데이터만 가져옴,
     String getUsername();
 }
